9c2d8d5605dc887e225366b618888f3bae3e4083
alifma <alifmaulana26@gmail.com>
2021-03-15 23:51:28 +0700

    Bootstrap Asset Folder

--- resources/views/v_guru.blade.php
+++ resources/views/v_guru.blade.php
@@ -2,4 +2,8 @@
 @section('title', 'Guru')
 @section('content')
 <h1>Ini Halaman Guru</h1>
+<div>
+    <img src="{{asset('img/image-1.png')}}" alt="" srcset=""><br>
+    <img src="{{asset('img/image-2.png')}}" alt="" srcset=""><br>
+</div>
 @endsection
